9a3c411874ee3503428f08f5217f6cc44e84f56d
AndIG0 <52193825+AndIG0@users.noreply.github.com>
2019-06-26 20:16:50 +0300

    Add files via upload
    The find of center of weight with Using  OpenCV

---
+++ Practik2019/CVUse.cpp
@@ -0,0 +1,82 @@
+﻿// CVUse.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include "opencv2/opencv.hpp"
+#include <stdio.h>
+#include <stdlib.h>
+
+			 using namespace cv;
+			 using namespace std;
+
+			 Mat src; Mat srcGray;
+			 RNG rng(12345);
+
+			 int main(int argc, char **argv)
+			 {
+				 
+				 src = imread("for_center.jpg", 1);
+
+				 
+				 cvtColor(src, srcGray, COLOR_BGR2GRAY);
+				 blur(srcGray, srcGray, Size(3, 3));
+
+				 Mat srcThresh;
+				 double otsu;
+
+				 otsu = threshold(srcGray, srcThresh, 0, 255, THRESH_BINARY | THRESH_OTSU);
+
+				 Mat cannyOut;
+				 Canny(srcGray, cannyOut, otsu, otsu * 1 / 2, 3, 1);
+
+				
+				 vector<vector<Point> > contours;
+				 vector<Vec4i> hierarchy;
+
+				 findContours(cannyOut, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point(0, 0));
+
+				 
+				 vector<Moments> mu(contours.size());
+				 for (int i = 0; i < contours.size(); i++)
+				 {
+					 mu[i] = moments(contours[i], false);
+				 }
+
+				 
+				 vector<Point2f> mc(contours.size());
+				 for (int i = 0; i < contours.size(); i++)
+				 {
+					 mc[i] = Point2f(mu[i].m10 / mu[i].m00, mu[i].m01 / mu[i].m00);
+				 }
+
+				 
+				 Mat drawing = Mat::zeros(cannyOut.size(), CV_8UC3);
+				 string sObjectNumber;         
+				 ostringstream sContourNumber;   
+
+				 for (int i = 0; i < contours.size(); i++)
+				 {
+					 
+					 sContourNumber << i;
+					 sObjectNumber = sContourNumber.str();   // Convert int to string
+					 Point pCoordinates(mc[i].x + 3, mc[i].y - 3);   // Text's coordinates (A little bit off from mass center)
+					 Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+					 drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, Point());
+					 circle(drawing, mc[i], 4, color, -1, 8, 0);     // Draw mass center
+					 putText(drawing, sObjectNumber, pCoordinates, FONT_HERSHEY_COMPLEX, 1, color, 2, 8); // Write object number
+					 sContourNumber.str("");     // Clear string
+					 sContourNumber.clear();     // Clear any error flags
+
+					 //        imshow("Contours", drawing);
+					 //        waitKey();
+				 }
+
+				 
+				 imshow("Gray", srcThresh);
+				 imshow("Canny",cannyOut);
+				 imshow("Contours", drawing);
+
+				 waitKey(0);
+				 return(0);
+			 }
